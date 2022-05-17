d0cb1f9983cd229dd1bb507ae57a0ec987574d84
Tawfiq Said <ltlx44@gmail.com>
2022-05-17 15:26:33 +0200

    Added beforeEach instead of repeating

--- src/test/java/boardgame/models/GameModelTest.java
+++ src/test/java/boardgame/models/GameModelTest.java
@@ -1,20 +1,27 @@
 package boardgame.models;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameModelTest {
 
+
+    GameModel model;
+
+    @BeforeEach
+    void setUp() {
+        model = new GameModel();
+    }
+
     @Test
     void squareProperty() {
-        GameModel model = new GameModel();
         assertThrows(IndexOutOfBoundsException.class,() -> model.squareProperty(GameModel.BOARD_SIZE, GameModel.BOARD_SIZE));
         assertEquals(new Piece(PieceType.EMPTY, new Position(4, 4)).getType(), model.squareProperty(GameModel.BOARD_SIZE - 1, GameModel.BOARD_SIZE - 1).getValue().getType());
     }
 
     @Test
     void click() {
-        GameModel model = new GameModel();
         model.click(0,0);
         assertEquals(model.getMoveCount(),1);
         assertEquals(model.squareProperty(0, 0).getValue().getType(), PieceType.RED);
@@ -22,7 +29,6 @@ class GameModelTest {
 
     @Test
     void getMoveCount() {
-        GameModel model = new GameModel();
         assertEquals(0, model.getMoveCount());
         model.click(0,0);
         model.click(0,1);
@@ -31,7 +37,6 @@ class GameModelTest {
 
     @Test
     void gameOver() {
-        GameModel model = new GameModel();
         model.click(0,0);
         Position pos = new Position(0,0);
         assertFalse(model.gameOver(pos, PieceType.RED));
@@ -45,7 +50,6 @@ class GameModelTest {
 
     @Test
     void isOnBoard() {
-        GameModel model = new GameModel();
         assertTrue(model.isOnBoard(0,0));
         assertFalse(model.isOnBoard(GameModel.BOARD_SIZE+1,GameModel.BOARD_SIZE+2));
         assertFalse(model.isOnBoard(-1,-1));
@@ -55,7 +59,6 @@ class GameModelTest {
 
     @Test
     void checkCol() {
-        GameModel model = new GameModel();
         assertThrows(ArrayIndexOutOfBoundsException.class,() -> model.checkCol(GameModel.BOARD_SIZE +1, PieceType.RED));
         model.click(0,0);
         assertFalse(model.checkCol(0,PieceType.RED));
@@ -65,7 +68,6 @@ class GameModelTest {
 
     @Test
     void checkRow() {
-        GameModel model = new GameModel();
         assertThrows(ArrayIndexOutOfBoundsException.class,() -> model.checkRow(GameModel.BOARD_SIZE -6, PieceType.RED));
         model.click(0,0);
         assertFalse(model.checkRow(0,PieceType.RED));
@@ -74,14 +76,12 @@ class GameModelTest {
 
     @Test
     void checkLongDiagonal() {
-        GameModel model = new GameModel();
         assertFalse(model.checkLongDiagonal(new Position(0, 1), PieceType.RED));
 
     }
 
     @Test
     void checkCell() {
-        GameModel model = new GameModel();
         assertFalse(model.checkCell(new Position(-1,0), new Position(1,1), PieceType.RED));
         assertTrue(model.checkCell(new Position(0,0), new Position(1,1), PieceType.EMPTY));
 
@@ -89,7 +89,6 @@ class GameModelTest {
 
     @Test
     void checkShortDiagonal() {
-        GameModel model = new GameModel();
         assertFalse(model.checkShortDiagonal(new Position(0, 1), PieceType.RED));
     }
 
